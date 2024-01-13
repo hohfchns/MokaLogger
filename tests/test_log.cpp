db77a1f735c47c8f2145142a8d4f04985640d19c
sahars <sahars@emg-soft.com>
2024-01-13 22:38:33 +0200

    Fixed absolute path in test

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -1,8 +1,19 @@
+#include <filesystem>
 #include <gtest/gtest.h>
 #include "moka/logger/logger.h"
 
+#define EXE_DIR_MAX_CHARACTERS 256
+static char g_exePath[EXE_DIR_MAX_CHARACTERS];
+
+#ifdef __linux__
+#define RETRIEVE_EXE_PATH() std::string __temp = std::filesystem::canonical("/proc/self/exe"); strcpy(g_exePath, __temp.c_str())
+#else
+#define RETRIEVE_EXE_PATH() GetModuleFileName(NULL, g_exePath, sizeof(EXE_DIR_MAX_CHARACTERS))
+#endif
+
 TEST(LoggerTest, BasicUsage) {
-  std::string path = "/home/hohfchns/Dev/C++/MokaLogger/logs/log.log";
+  RETRIEVE_EXE_PATH();
+  std::string path = std::filesystem::path(std::string(g_exePath)).parent_path().string() + "/log.log";
   auto config = moka::log::Logger::activeLogger->GetConfig();
   config.filePath = path;
   moka::log::Logger::activeLogger->SetConfig(config);
